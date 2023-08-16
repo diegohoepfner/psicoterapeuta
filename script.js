f4b7af5cd3fbd94a5341d502ba615d3730e9df0e
diegohoepfner <136132378+diegohoepfner@users.noreply.github.com>
2023-08-16 15:32:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const chatLog = document.getElementById("chat-log");
 const userInput = document.getElementById("user-input");
 const sendButton = document.getElementById("send-btn");
 
-function sendMessage() {
+async function sendMessage() {
     const userMessage = userInput.value;
     userInput.value = "";
 
@@ -10,36 +10,39 @@ function sendMessage() {
     chatLog.innerHTML += `<div class="user-message">${userMessage}</div>`;
 
     // Faça uma chamada à API do ChatGPT
-    fetch("https://api.openai.com/v1/chat/completions", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer 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"
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "system", content: "Você é um psicoterapeuta rogeriano que simula o comportamento do chatbot Eliza" }, { role: "user", content: userMessage }]
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        const botReply = data.choices[0].message.content;
+    try {
+        const response = await fetch("https://api.openai.com/v1/engines/gpt-3.5-turbo/completions", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": "Bearer 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"
+			},
+			body: JSON.stringify({
+				model: "gpt-3.5-turbo",
+				messages: [{ role: "system", content: "Você é um psicoterapeuta rogeriano que simula o comportamento do chatbot Eliza" }, { role: "user", content: userMessage }]
+			})
+		});
+
+        const data = await response.json();
+        const botReply = data.choices[0].text;
 
         // Adicione a resposta do bot ao chat
         chatLog.innerHTML += `<div class="bot-message">${botReply}</div>`;
 
-        // Role o chat automaticamente para exibir a última mensagem
+	    // Role o chat automaticamente para exibir a última mensagem
         chatLog.scrollTop = chatLog.scrollHeight;
-    });
+    } catch (error) {
+        console.error("Erro ao fazer a chamada à API:", error);
+    }
 }
 
 sendButton.addEventListener("click", () => {
     sendMessage();
 });
 
-userInput.addEventListener("keydown", event => {
+userInput.addEventListener("keydown", async event => {
     if (event.key === "Enter" && !event.shiftKey) {
         event.preventDefault(); // Evita a quebra de linha padrão do Enter
-        sendMessage();
+        await sendMessage();
     }
 });
